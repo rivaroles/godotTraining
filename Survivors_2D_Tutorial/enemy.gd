4ab8412ccd385a94640500ac2470c7ec1db6691c
Lucas Dos Santos Barreto Rivarola Da Silva <estagio.startup26@sesims.com.br>
2024-06-27 13:44:47 -0400

    sistema de tiro por tempo e dano implementado

--- Survivors_2D_Tutorial/enemy.gd
+++ Survivors_2D_Tutorial/enemy.gd
@@ -1,9 +1,28 @@
 extends CharacterBody2D
 
-@onready var player = %Player
+var health = 3
+
+@onready var player = $"../Player"
+@onready var hurtbox_collision = $CollisionShape2D
+@onready var damage_numbers_origin = $DamageNumbersOrigin
+
+func _ready():
+	%Slime.play_walk()
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 300.0
+	velocity = direction * 100.0
 	move_and_slide()
 
+func take_damage():
+	health -= 1
+	DamageNumbers.display_number(health, damage_numbers_origin.global_position)
+	%Slime.play_hurt()
+	
+	if health == 0:
+		queue_free()
+		
+		const SMOKE_EXPLOSION = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_EXPLOSION.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
--- Survivors_2D_Tutorial/player.gd
+++ Survivors_2D_Tutorial/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 @onready var sprite = %HappyBoo
+var health = 100.0
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -17,3 +20,10 @@ func _physics_process(_delta):
 	elif Input.is_action_just_pressed("move_right"):
 		%HappyBoo.scale.x = 1
 	
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * _delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
